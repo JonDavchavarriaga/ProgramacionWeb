a2789043c728ac5e8568943f25e32e6c1c2b7385
Jonatan <jonatanchavarriaga@gmail.com>
2023-05-17 16:40:18 -0500

    Se  Hace ejericio de consumo de Api

---
+++ indexApi.js
@@ -0,0 +1,26 @@
+function getCharacters(done)
+{
+    const results= fetch("https://rickandmortyapi.com/api/character");
+
+    results.then(response => response.json())
+    .then(data => {
+        done(data)
+    } );
+}
+getCharacters(data => {
+    data.results.forEach(personaje => {
+        const article= document.createRange().createContextualFragment(
+        `
+        <article>
+        <div class="imsge-container">
+            <img src="${personaje.image}" alt="Personaje">
+        </div>
+        <h2>${personaje.name}</h2>
+        <span>${personaje.status}</span>
+    </article>
+        `
+        );
+        const main = document.querySelector("main");
+        main.append(article);
+    });
+} );
